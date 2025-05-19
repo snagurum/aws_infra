57b13b91b0986b2e00743e16880aac784fb03547
Murugan Sundaram <murugan.sundaram@ideas2it.com>
2025-05-19 12:47:53 +0530

    test19

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -3,3 +3,10 @@ resource "local_file" "foo" {
   filename = "${path.module}/foo.bar"
 }
 
+resource "random_uuid" "test" {
+}
+
+output "uuid" {
+  description = "uuid"
+  value       = try(random_uuid.id, null)
+}
